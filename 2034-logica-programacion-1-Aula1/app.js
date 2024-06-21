444156d19e11caa2e54f4d2e35e03fe87962ed94
juespega <juespega666@gmail.com>
2024-06-21 07:56:04 -0500

    Aula1

---
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -0,0 +1,33 @@
+/* let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+} */
+
+/*let numeroSecreto = 6;
+let numeroUsuario = prompt('Me indicas un numero por favor: ');
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}*/
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+var nombre = 'Luna';
+console.log(nombre);
+let edad = 25;
+console.log(edad);
+let numeroDeVentas = 50;
+console.log(numeroDeVentas);
+alert("¡Error! Completa todos los campos");
+var mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+nombre = prompt("Por favor, ingresa tu nombre");
+edad = prompt("Por favor, ingresa tu edad");
+if (edad >= 18) {
+    alert("¡Puedes obtener tu licencia de conducir!");
+}
